fecb3ac2db66a9e2375a112198afca5fb9655f5c
esquire-gh <ackon.k.richard@gmail.com>
2019-03-04 20:15:52 +0000

    base settings refactor

--- mpharma_challenge/settings/base.py
+++ mpharma_challenge/settings/base.py
@@ -29,7 +29,7 @@ environ.Env.read_env(env_file=ENV_FILE)
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY'] or env('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
